eee7d2535850ac6a9fbb59eb6672d551f1f75a66
Kawin Glomjai <g.kawin@live.com>
2024-06-22 19:05:37 -0700

    hello world

--- airbnb-ai-website/src/app/page.tsx
+++ airbnb-ai-website/src/app/page.tsx
@@ -1,3 +1,13 @@
+import getPocketbase from "@/helpers/pocketbase";
+
 export default async function Home() {
-  return <main></main>;
+  const pb = await getPocketbase();
+
+  const allusers = await pb.health.check();
+
+  return (
+    <main className="bg-white">
+      <pre>{JSON.stringify(allusers, null, 2)}</pre>
+    </main>
+  );
 }
