64d6bfc560d5f28af1e632e43514d07b3919670e
Kawin Glomjai <g.kawin@live.com>
2024-06-22 19:09:17 -0700

    deployment

--- airbnb-ai-website/src/app/page.tsx
+++ airbnb-ai-website/src/app/page.tsx
@@ -4,10 +4,14 @@ export default async function Home() {
   const pb = await getPocketbase();
 
   const allusers = await pb.health.check();
+  const baseUrl = pb.baseUrl;
 
   return (
     <main className="bg-white">
-      <pre>{JSON.stringify(allusers, null, 2)}</pre>
+      <pre>
+        {JSON.stringify(allusers, null, 2)}
+        {baseUrl}
+      </pre>
     </main>
   );
 }
